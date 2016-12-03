3098c3f33886382573c421d20eb0cff0ebe8d8d4
Mello578 <mello.shlyapa@gmail.com>
2016-12-04 01:28:42 +0500

    Update java.js

--- java.js
+++ java.js
@@ -1,18 +1,3 @@
-//var sizeM =0;
-/*function ask () {
-    var sizeM = document.getElementById("sizeMatrix").value;
-
-    /*  for (var i=0; i<sizeM; i++)
-     {
-     <input type="number" id=i> ;
-
-    if (sizeM>0)
-    {
-        alert (sizeM);
-    
-    }  
-}*/
-
 var name;
 var consum=0, consum2=0;
 name = prompt("Введи своё беспонтовое имя");
@@ -20,21 +5,22 @@ name2= prompt ("Введи имя второго лоха");
 
 function consumption ()
 {
-    var distance = document.getElementById("dist").value;
-    var spent = document.getElementById("cpa").value;
+    var distance = document.getElementById("dist").value=0;
+    var spent = document.getElementById("cpa").value=0;
     consum=spent/distance*100;
     alert (name + " Твоя помойка на 100 км жрет " + consum + " литров бензина");
     return consum;
 }
 function consumption2 ()
 {
-    var distance2 = document.getElementById("dist2").value;
-    var spent2 = document.getElementById("cpa2").value;
+    var distance2 = document.getElementById("dist2").value=0;
+    var spent2 = document.getElementById("cpa2").value=0;
     consum2=spent2/distance2*100;
     alert ("А помойка лоха на 100 км жрет " + consum2 + " литров бензина");
     return consum2;
 }
-lohan(consum, consum2)
+    lohan(consum, consum2)
+/*
 function lohan (a, b)
 {
  if (a>b)
