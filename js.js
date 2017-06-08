1eff5b4883012f2619d62b497463b2649521fa12
Mello578 <mello.shlyapa@gmail.com>
2017-06-08 18:20:12 +0500

    Пятнахи

--- js.js
+++ js.js
@@ -2,8 +2,10 @@
  * Created by Sysadmin on 18.05.2017.
  */
 
-let rows = [], rowsFinish = [];
+let rows = [], etalonRows = [];
 
+
+// создаем таблицу
 window.onload = function () {
     var table = document.createElement('table'),
         fragment = document.createDocumentFragment(),
@@ -29,27 +31,123 @@ window.onload = function () {
     $('td').css('height', '127px');
     $('td').css('width', '132px');
 
-    $(rows[0][0]).append('<img id="1" src="img/1.png">');
-    $(rows[0][1]).append('<img id="2" src="img/2.png">');
-    $(rows[0][2]).append('<img id="3" src="img/3.png">');
-    $(rows[0][3]).append('<img id="4" src="img/4.png">');
-    $(rows[1][0]).append('<img id="5" src="img/5.png">');
-    $(rows[1][1]).append('<img id="6" src="img/6.png">');
-    $(rows[1][2]).append('<img id="7" src="img/7.png">');
-    $(rows[1][3]).append('<img id="8" src="img/8.png">');
-    $(rows[2][0]).append('<img id="9" src="img/9.png">');
-    $(rows[2][1]).append('<img id="10" src="img/10.png">');
-    $(rows[2][2]).append('<img id="11" src="img/11.png">');
-    $(rows[2][3]).append('<img id="12" src="img/12.png">');
-    $(rows[3][0]).append('<img id="13" src="img/13.png">');
-    $(rows[3][1]).append('<img id="14" src="img/14.png">');
-    $(rows[3][2]).append('<img id="15" src="img/15.png">');
-    $(rows[3][3]).append('');
+// создаем массив из пятнашек
+    let specks = [{
+        id: 1,
+        speck: '<img src="img/1.png">'
+    }, {
+        id: 2,
+        speck: '<img src="img/2.png">'
+    }, {
+        id: 3,
+        speck: '<img src="img/3.png">'
+    }, {
+        id: 4,
+        speck: '<img src="img/4.png">'
+    }, {
+        id: 5,
+        speck: '<img src="img/5.png">'
+    }, {
+        id: 6,
+        speck: '<img src="img/6.png">'
+    }, {
+        id: 7,
+        speck: '<img src="img/7.png">'
+    }, {
+        id: 8,
+        speck: '<img src="img/8.png">'
+    }, {
+        id: 9,
+        speck: '<img src="img/9.png">'
+    }, {
+        id: 10,
+        speck: '<img src="img/10.png">'
+    }, {
+        id: 11,
+        speck: '<img src="img/11.png">'
+    }, {
+        id: 12,
+        speck: '<img src="img/12.png">'
+    }, {
+        id: 13,
+        speck: '<img src="img/13.png">'
+    }, {
+        id: 14,
+        speck: '<img src="img/14.png">'
+    }, {
+        id: 15,
+        speck: '<img src="img/15.png">'
+    }, {
+        id: 0,
+        speck: ''
+    }];
 
-    rowsFinish = rows;
-};
+    //массив с правильным расположением ячеек
+    for (let key in specks){
+        etalonRows.push(specks[key]);
+    }
+
+// заполняем поле пятнашками
+    (function () {
+        let oneSpeck, countSpeck = 0;
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                oneSpeck = specks[countSpeck];
+                $(rows[i][j]).append(oneSpeck.speck);
+                countSpeck++;
+            }
+        }
+        }());
+
+    //перемешивание ячеек
+
+    function shufle() {
+
+        let summ = 0;
+        for (let i = 0; i < 1024; i++) {
+            for (let key in rows) {
+                let stringCells = rows[key];
+                for (let key2 in stringCells) {
+                    let columsCells = stringCells[key2];
+                    if ($(columsCells).html() == '') {
+                        let randY = Math.round(3 * Math.random()),
+                            randX = Math.round(3 * Math.random());
+                        if (randY != key && randX != key2) {
+                            let content = $(rows[randY][randX]).html();
+                            $(rows[key][key2]).append(content);
+                            $(rows[randY][randX]).html('');
+                        }
+                    }
+                }
+            }
+        }
+        //проверка на сходимость
+        let shufleTable = [];
+        for (let key in rows) {
+            let stringCells = rows[key];
+            for (let key in stringCells) {
+                let cells = stringCells[key];
+                for (let key in specks) {
+                    cells.innerHTML === specks[key].speck ? shufleTable.push(specks[key].id) : shufleTable;
+                }
+            }
+        }
+        for (let i = 0; i < shufleTable.length; i++) {
+            let shufleId = shufleTable[i];
+            for (let j = i + 1; j < shufleTable.length; j++) {
+                shufleTable[j] != 0 && shufleTable[j] < shufleId ? summ++ : summ + 0;
+            }
+        }
+        (summ + 1) % 2 !== 0 ? shufle() : summ;
+    }
+
+    // старт игры
+    document.getElementById('START').onclick = function start() {
+        shufle();
+    }
+
+}
 let x, y, number;
-
 
 $(function () {
 
@@ -62,68 +160,42 @@ $(function () {
     });
 });
 
-
 function motionCell(axisY, axisX, content) {
-    if (content !== '') {
-        if (axisX !== 3 && $(rowsFinish[axisY][axisX + 1]).html() == '') {
-            $(rowsFinish[axisY][axisX]).html('');
-            $(rowsFinish[axisY][axisX + 1]).append(content);
+    if (content != "") {
+        if (axisX !== 3 && $(rows[axisY][axisX + 1]).html() == "") {
+            $(rows[axisY][axisX]).html("");
+            $(rows[axisY][axisX + 1]).append(content);
         }
-        if (axisX !== 0 && $(rowsFinish[axisY][axisX - 1]).html() == '') {
-            $(rowsFinish[axisY][axisX]).html('');
-            $(rowsFinish[axisY][axisX - 1]).append(content);
+        if (axisX !== 0 && $(rows[axisY][axisX - 1]).html() == "") {
+            $(rows[axisY][axisX]).html("");
+            $(rows[axisY][axisX - 1]).append(content);
         }
 
-        if (axisY !== 3 && $(rowsFinish[axisY + 1][axisX]).html() == '') {
-            $(rowsFinish[axisY][axisX]).html('');
-            $(rowsFinish[axisY + 1][axisX]).append(content);
+        if (axisY !== 3 && $(rows[axisY + 1][axisX]).html() == "") {
+            $(rows[axisY][axisX]).html("");
+            $(rows[axisY + 1][axisX]).append(content);
         }
-        if (axisY !== 0 && $(rowsFinish[axisY - 1][axisX]).html() == '') {
-            $(rowsFinish[axisY][axisX]).html('');
-            $(rowsFinish[axisY - 1][axisX]).append(content);
+        if (axisY !== 0 && $(rows[axisY - 1][axisX]).html() == "") {
+            $(rows[axisY][axisX]).html("");
+            $(rows[axisY - 1][axisX]).append(content);
         }
     }
 }
 
-document.getElementById('START').onclick = function start() {
-    for (let i = 0; i < 1024; i++) {
-        for (let key in rowsFinish) {
-            let str = rowsFinish[key];
-            for (let key2 in str) {
-                let noElem = str[key2];
-                if ($(noElem).html() == '') {
-                    let randY = Math.round(3 * Math.random()),
-                        randX = Math.round(3 * Math.random());
-                    //Переставление пятнашек с проверкой на решаемость
-                    if (randY != key && randX != key2) {
-                        let content = $(rowsFinish[randY][randX]).html();
-                        $(rowsFinish[key][key2]).append(content);
-                        $(rowsFinish[randY][randX]).html('');
-                    }
-                }
-            }
-        }
-    }
-
-
-};
-
 document.getElementById('FINISH').onclick = function finish() {
-    let counter = 0;
-    for (let i = 0; i < 4; i++) {
-        let str = rowsFinish[i];
-        for (let j = 0; j < 4; j++) {
-            let tablSt = $(rowsFinish[i][j]).html(),
-                tablFin = $(rows[i][j]).html();
-            if (tablSt === tablFin) {
-                counter++;
-            }
+    let counter = 0, finishRows = [];
+    for (let key in rows) {
+        for (let key2 in rows[key]){
+            finishRows.push(rows[key][key2].innerHTML);
         }
-    }
+            }
+            for (let key in finishRows){
+                finishRows[key] === etalonRows[key].speck ? counter++ : counter;
+            }
+
     if (counter === 16) {
         alert('Вы победили');
     } else {
         alert('Что то тут не так');
     }
 };
-
