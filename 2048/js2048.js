eaaf583caadf50d1e26a26cfa23554d973ef8064
mello578 <mello.shlyapa@gmail.com>
2017-06-21 17:58:46 +0600

    1

--- 2048/js2048.js
+++ 2048/js2048.js
@@ -294,8 +294,29 @@ document.body.addEventListener("keydown", function (event) {
     }
 });
 
-var supportsTouch = ('ontouchstart' in document.documentElement);
-while (supportsTouch){
+isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+
+while (isMobile.any()){
     $(function () {
         var initialPoint;
         var finalPoint;
